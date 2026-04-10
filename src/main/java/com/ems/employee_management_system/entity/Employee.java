98185ed0af2d0bff9d2e329faf71316fa0b999b8
Hafeez <syedhafeez957@gmail.com>
2026-04-10 15:11:28 +0530

    added employee entity class

--- src/main/java/com/ems/employee_management_system/entity/Employee.java
+++ src/main/java/com/ems/employee_management_system/entity/Employee.java
@@ -1,4 +1,30 @@
 package com.ems.employee_management_system.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer empid;
+    private String name;
+    private String email;
+    private double salary;
+    private String department;
+    private String address;
+    @CreationTimestamp
+    private LocalDateTime creation;
+    @UpdateTimestamp
+    private LocalDateTime updation;
+
 }
